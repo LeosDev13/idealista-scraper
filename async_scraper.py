4f66eda48c5bc47a2d1b5f9728c36b3c38762b37
Leos1113 <edgarleonmartin13@gmail.com>
2025-02-22 19:20:39 +0100

    refactor the main script to a class

--- async_scraper.py
+++ async_scraper.py
@@ -3,66 +3,66 @@ import time
 import random
 from bs4 import BeautifulSoup
 from curl_cffi.requests import AsyncSession
-
+from db import Db
 from utils import parse_price
 
 BASE_URL = "https://www.idealista.com"
-semaphore = asyncio.Semaphore(3)
 
 
-async def fetch_property_details(session, property_url):
-    async with semaphore:
-        response = await session.get(f"{BASE_URL}{property_url}")
-        soup = BeautifulSoup(response.text, 'lxml')
-        price_text = soup.find("span", class_="info-data-price")
-        if price_text is None:
-            print(f"❌ price text not found for property {property_url}")
+class IdealistaScraper:
+    def __init__(self):
+        self.semaphore = asyncio.Semaphore(3)
+        self.db = Db()
+        self.session = None
+
+    async def fetch_property_details(self, session, property_url):
+        async with self.semaphore:
+            response = await session.get(f"{BASE_URL}{property_url}")
+            soup = BeautifulSoup(response.text, 'lxml')
+            price_text = soup.find("span", class_="info-data-price")
+            if price_text is None:
+                print(f"❌ price text not found for property {property_url}")
+                return
+            price = parse_price(price_text.get_text())
+            print(f"🏠 {property_url} → Precio: {price}")
+            await asyncio.sleep(random.uniform(5, 15))
+
+    async def fetch_property_links(self, session, soup):
+        return [link.get('href') for link in soup.find_all("a", class_="item-link")]
+
+    async def get_next_page_link(self, session, soup):
+        next_li = soup.find("li", class_="next")
+        if next_li is None:
+            print("❌ failed to find next li")
             return
-        price = parse_price(price_text.get_text())
-        print(f"🏠 {property_url} → Precio: {price}")
-        await asyncio.sleep(random.uniform(5, 15))
+        next_path = next_li.find("a").get("href")
+        return f"{BASE_URL}{next_path}"
 
+    async def run(self):
+        start_time = time.time()
+        print("🔍 Scraping Idealista asynchronous...")
+        initial_url = "https://www.idealista.com/geo/venta-viviendas/rodalies-premia-de-mar/con-precio-hasta_600000,metros-cuadrados-mas-de_120,chalets/"
 
-async def fetch_property_links(session, soup):
-    return [link.get('href') for link in soup.find_all("a", class_="item-link")]
+        async with AsyncSession(impersonate="chrome") as session:
+            await self.scrape_page(initial_url, session)
 
+        print(f"✅ Tiempo total: {time.time() - start_time:.2f} segundos")
 
-async def get_next_page_link(session, soup):
-    next_li = soup.find("li", class_="next")
-    if next_li is None:
-        print("❌ failed to find next li")
-        return
-    next_path = next_li.find("a").get("href")
-    print("✅ next page")
-    return f"{BASE_URL}{next_path}"
+    async def scrape_page(self, url: str | None, session):
+        if url is None:
+            return
+        r = await session.get(url)
+        soup = BeautifulSoup(r.text, "lxml")
 
+        property_links = await self.fetch_property_links(session, soup)
 
-# 🏡 Función principal asíncrona
-async def main():
-    start_time = time.time()
-    print("🔍 Scraping Idealista asynchronous...")
-    initial_url = "https://www.idealista.com/geo/venta-viviendas/rodalies-premia-de-mar/con-precio-hasta_600000,metros-cuadrados-mas-de_120,chalets/"
+        await asyncio.gather(*[self.fetch_property_details(session, link) for link in property_links])
 
-    async with AsyncSession(impersonate="chrome") as session:
-        await run_scraper(initial_url, session)
+        next_page_link = await self.get_next_page_link(session, soup)
 
-    print(f"✅ Tiempo total: {time.time() - start_time:.2f} segundos")
-
-
-async def run_scraper(url: str | None, session):
-    if url is None:
-        return
-    r = await session.get(url)
-    soup = BeautifulSoup(r.text, "lxml")
-
-    property_links = await fetch_property_links(session, soup)
-
-    await asyncio.gather(*[fetch_property_details(session, link) for link in property_links])
-
-    next_page_link = await get_next_page_link(session, soup)
-
-    await run_scraper(next_page_link, session)
+        await self.run_scraper(next_page_link, session)
 
 
 if __name__ == "__main__":
-    asyncio.run(main())
+    scraper = IdealistaScraper()
+    asyncio.run(scraper.run())
