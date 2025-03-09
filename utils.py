dea94263c8ea915c7592e5398810e9c9f82f8505
Leos1113 <edgarleonmartin13@gmail.com>
2025-03-09 20:20:32 +0100

    refactor: move constants to his own file
    Create a helper function to map location ids from idealista to text

--- utils.py
+++ utils.py
@@ -1,18 +1,22 @@
 from decimal import Decimal
 from Money import Money
 import re
+from constants import LOCATIONS_MAP, CURRENCY_MAP
 
 
 def parse_price(price_str):
-    match = re.match(r'([\d.,]+)\s*([€$£]?)', price_str)
+    match = re.match(r"([\d.,]+)\s*([€$£]?)", price_str)
     if not match:
         raise ValueError(f"Formato no válido: {price_str}")
 
     raw_amount, currency_symbol = match.groups()
-    normalized_amount = raw_amount.replace('.', '').replace(',', '.')
+    normalized_amount = raw_amount.replace(".", "").replace(",", ".")
     amount = Decimal(normalized_amount)
 
-    currency_map = {'€': 'EUR', '$': 'USD', '£': 'GBP'}
-    currency = currency_map.get(currency_symbol, 'UNKNOWN')
+    currency = CURRENCY_MAP.get(currency_symbol, "UNKNOWN")
 
     return Money(amount=amount, currency=currency)
+
+
+def get_location_from_id(location_id: str) -> str:
+    return LOCATIONS_MAP.get(location_id)
