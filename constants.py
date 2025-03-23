618b6f45cd9aa7527bff256e7d107ad2409e909a
Leos1113 <edgarleonmartin13@gmail.com>
2025-03-23 23:04:57 +0100

    Move base url for locations
    Move the base url for locations to constants folder

--- constants.py
+++ constants.py
@@ -1,3 +1,6 @@
 LOCATION_MAP = {"0-EU-ES-35-03-002-034-02-001": "Montaña Roja, Yaiza"}
 CURRENCY_MAP = {"€": "EUR", "$": "USD", "£": "GBP"}
 BASE_URL = "https://www.idealista.com"
+LOCATIONS_BASE_URL = (
+    "https://www.idealista.com/es/locationsSuggest/sale/home?searchField="
+)
