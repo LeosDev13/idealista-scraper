067916a403aea7586ca5189609f128850e41345b
LeosDev13 <leosdev13@gmail.com>
2025-04-25 20:48:24 +0200

    clean imports

--- async_scraper.py
+++ async_scraper.py
@@ -1,22 +1,18 @@
 import asyncio
-import re
 import time
 import random
 from bs4 import BeautifulSoup
 from curl_cffi.requests import AsyncSession
 
-from decimal import Decimal
-
 from utils import parse_price
-from Money import Money
 
 BASE_URL = "https://www.idealista.com"
 semaphore = asyncio.Semaphore(3)
 
-# 🔄 Función asíncrona para obtener los detalles de una propiedad
+
 async def fetch_property_details(session, property_url):
     async with semaphore:
-        response = await  session.get(f"{BASE_URL}{property_url}")
+        response = await session.get(f"{BASE_URL}{property_url}")
         soup = BeautifulSoup(response.text, 'lxml')
         price_text = soup.find("span", class_="info-data-price")
         if price_text is None:
@@ -30,6 +26,7 @@ async def fetch_property_details(session, property_url):
 # 🔍 Función para extraer enlaces de propiedades de una página
 async def fetch_property_links(session, soup):
     return [link.get('href') for link in soup.find_all("a", class_="item-link")]
+
 
 async def get_next_page_link(session, soup):
     next_li = soup.find("li", class_="next")
@@ -45,11 +42,11 @@ async def get_next_page_link(session, soup):
 async def main():
     start_time = time.time()
     print("🔍 Scrapeando Idealista de forma asíncrona...")
-    initial_url =  "https://www.idealista.com/geo/venta-viviendas/rodalies-premia-de-mar/con-precio-hasta_600000,metros-cuadrados-mas-de_120,chalets/"
+    initial_url = "https://www.idealista.com/geo/venta-viviendas/rodalies-premia-de-mar/con-precio-hasta_600000,metros-cuadrados-mas-de_120,chalets/"
 
     async with AsyncSession(impersonate="chrome") as session:
         await run_scraper(initial_url, session)
-        
+
     print(f"✅ Tiempo total: {time.time() - start_time:.2f} segundos")
 
 
@@ -58,7 +55,7 @@ async def run_scraper(url: str | None, session):
         return
     r = await session.get(url)
     soup = BeautifulSoup(r.text, "lxml")
-    
+
     property_links = await fetch_property_links(session, soup)
 
     await asyncio.gather(*[fetch_property_details(session, link) for link in property_links])
@@ -66,7 +63,6 @@ async def run_scraper(url: str | None, session):
     next_page_link = await get_next_page_link(session, soup)
 
     await run_scraper(next_page_link, session)
-
 
 
 # 🚀 Ejecutar el script asíncrono
